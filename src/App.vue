91063769ebd7c8100f55df405bf555912d275c53
Prabhakar Acharya <acharyaP03@gmail.com>
2021-02-25 14:50:12 +1100

    Header Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="app" class="container">
+  <div id="app" class="container-fluid">
+    <app-header></app-header>
     <router-view />
   </div>
 </template>
 
+<script>
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    appHeader: Header,
+  },
+};
+</script>
 <style>
 #app {
   font-family: "Montserrat", sans-serif;
@@ -11,5 +20,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 0;
 }
 </style>
