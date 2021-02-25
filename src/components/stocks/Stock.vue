d4bd25bd6fe5add2d2b76bbb352468f2f2c33287
Prabhakar Acharya <acharyaP03@gmail.com>
2021-02-25 15:43:08 +1100

    Stock form

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="home">
-    <h1>The Stock - stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card text-white  mb-3">
+      <div class="card-header bg-success">
+        <h3 class="card-title">NAME <small>(Price: PRICE)</small></h3>
+      </div>
+      <div class="card-body">
+        <div class="input-group">
+          <input type="text" class="form-control" id="password" placeholder="Quantity...." />
+          <button class="btn btn-success">Buy</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
