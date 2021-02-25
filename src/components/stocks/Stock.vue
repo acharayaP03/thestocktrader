1c29dad79d7370e2c04e002c0a1dc6572cb0d638
acharyaP03 <acharyap03@gmail.com>
2021-02-25 16:21:11 +1100

    Trial for updating remote branch from diffrent machine

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="col-sm-6 col-md-4">
-    <div class="card text-white  mb-3">
+    <div class="mb-3 text-white card">
       <div class="card-header bg-success">
-        <h3 class="card-title">NAME <small>(Price: PRICE)</small></h3>
+        <h5 class="card-title">{{stock.name}} <small>(Price: {{stock.price}})</small></h5>
       </div>
       <div class="card-body">
         <div class="input-group">
-          <input type="text" class="form-control" id="password" placeholder="Quantity...." />
-          <button class="btn btn-success">Buy</button>
+          <input type="number" class="form-control" id="password" placeholder="Quantity...." v-model="quantity"/>
+          <button class="btn btn-success" @click="buyStock">Buy</button>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  props:{
+    stock:{
+      type: Object,
+      required: true
+    }
+  },
+  data(){
+    return{
+      quantity: 0
+    }
+  },
+  methods:{
+    buyStock(){
+
+      const order = {
+        stockId: new Date().toISOString(),
+        stockQuantity: this.quantity,
+        stockPrice: this.stock.price
+      }
+
+      console.log(order);
+      this.quantity = 0
+    }
+  }
+}
+</script>
