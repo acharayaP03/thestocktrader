8699d561bd476713d89923a6fb7746c0f86fcccc
Prabhakar Acharya <acharyaP03@gmail.com>
2021-04-13 16:26:57 +1000

    add endday methods and action in header

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -1,42 +1,44 @@
-import stocks from '../../data/stocks';
+import stocks from "../../data/stocks";
 
-const state ={
-  stocks: []
-}
+const state = {
+  stocks: [],
+};
 
 const mutations = {
-  'SET_STOCKS'(state, stocks) {
-    state.stocks = stocks
+  SET_STOCKS(state, stocks) {
+    state.stocks = stocks;
   },
-  'RND_STOCKS'(){
+  RND_STOCKS() {
+    state.stocks.forEach((stock) => {
+      stock.price = Math.round(stock.price * (1 + Math.random() - 0.5));
+    });
+  },
+};
 
-  }
-}
-
-const actions ={
-  buyStocks: ({ commit }, order) =>{
+const actions = {
+  buyStocks: ({ commit }, order) => {
     //BUY_STOCKS reffers to the global name space of portfolio.
     //note: store is available across the applications and can be accessed from anywhere.
-    commit('BUY_STOCKS', order);
+    commit("BUY_STOCKS", order);
   },
-  setStocks: ({ commit })=>{
+  setStocks: ({ commit }) => {
     //stocks is the data that we are receiveing from data/stocks.js
-    commit('SET_STOCKS', stocks);
+    commit("SET_STOCKS", stocks);
   },
-  randomizeStocks: ( { commit }) =>{
-    commit('RND_STOCKS');
-  }
-}
+  randomizeStocks: ({ commit }) => {
+    commit("RND_STOCKS");
+  },
+};
 
-const getters ={
-  stocks: state =>{
+const getters = {
+  stocks: (state) => {
     return state.stocks;
-  }
-}
+  },
+};
 
 export default {
   state,
   mutations,
   actions,
-  getters
-}
+  getters,
+};
